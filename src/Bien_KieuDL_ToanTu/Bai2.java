ea948cd901cc88d8ef315fa0af1c96155edfe47e
khoa developer <khoandph05225@fpt.edu.vn>
2018-03-16 15:32:04 +0700

    commit bien dieu kien toan tu

---
+++ src/Bien_KieuDL_ToanTu/Bai2.java
@@ -0,0 +1,12 @@
+package Bien_KieuDL_ToanTu;
+
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào độ Celsius :");
+        int c = sc.nextInt();
+        System.out.println(c +" Celsius = "+(9*c)/5+32+" Fahrenheit");
+    }
+}
---
+++ src/Bien_KieuDL_ToanTu/ChuyenDoiTienTe.java
@@ -0,0 +1,13 @@
+package Bien_KieuDL_ToanTu;
+
+import java.util.Scanner;
+
+public class ChuyenDoiTienTe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER money (USD) :");
+        float tienUSD = sc.nextFloat();
+        float tienVND = tienUSD*23000;
+        System.out.println(tienUSD+"USD = "+tienVND);
+    }
+}
